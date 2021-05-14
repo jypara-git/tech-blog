1173de913f624f6f040abcb9f9096c0652e6e2f4
jypara-git <zhyparaabdieva@gmail.com>
2021-05-14 15:28:14 -0700

    set up the server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+//const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
