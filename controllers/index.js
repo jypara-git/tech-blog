43e6ea8f62b427ffca27cd5d29e7606bebd2dbe9
jypara-git <zhyparaabdieva@gmail.com>
2021-05-24 15:12:49 -0700

    connected front end to the back end

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const homeRoutes = require('./home-routes');
-const dashboardRoutes = require('./dasboard-routes');
+const dashboardRoutes = require('./dashboard-routes');
 
 const apiRoutes = require('./api ');
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
-router.use('/dasboard', dashboardRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const express = require('express');
-//const routes = require('./controllers');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
-
+const path = require('path');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({helpers});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//app.use(routes);
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUnitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+app.use(session(sess));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
